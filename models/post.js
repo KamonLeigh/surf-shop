5c1059c39c8e1ea58f8cf118e5eb70dbb7508b21
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2019-03-18 01:48:04 +0000

    added models for user review and post

---
+++ models/post.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    images: [String],
+    location: String,
+    lat: Number,
+    lng: Number,
+    author: {
+        type:Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+});
+
+
+module.exports = mongoose.model('Post', PostSchema);
+
+
+
+
+/* 
+    Post
+    - title - string
+    - price - string
+    - decription - string
+    - images - an array of strings
+    - location - string
+    - lat - number
+    - long - number 
+    - author - object id (ref user)
+    - reviews array of objects
+
+*/
